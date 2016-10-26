a8407b4e40f56eabcf2de9303ea9f17c4566232f
yswhynot <yswhynot@gmail.com>
2016-10-26 21:33:02 +0800

    update

--- laser_transform/include/laser_transform/laser_transform.h
+++ laser_transform/include/laser_transform/laser_transform.h
@@ -1,6 +1,11 @@
 #include <ros/ros.h>
+#include <Eigen/Dense>
 #include <tf/transform_listener.h>
 #include <laser_geometry/laser_geometry.h>
+#include "pcl/common/eigen.h"
+#include "pcl/common/transforms.h"
+
+const double M_PI = 3.1415926;
 
 class My_Filter {
 public:
@@ -13,10 +18,37 @@ private:
 
     ros::Publisher point_cloud_publisher_;
     ros::Subscriber scan_sub_;
+
+    Eigen::Matrix4f rotMatrixX;
+    Eigen::Matrix4f rotMatrixY;
+    Eigen::Matrix4f rotMatrixZ;
 };
 
 My_Filter::My_Filter(){
     scan_sub_ = node_.subscribe<sensor_msgs::LaserScan> ("/bookle/laser/scan", 100, &My_Filter::scanCallback, this);
     point_cloud_publisher_ = node_.advertise<sensor_msgs::PointCloud2> ("/velodyne_points", 100, false);
     tfListener_.setExtrapolationLimit(ros::Duration(0.1));
+
+    pcl::PointCloud<pcl::pointxyzrgb> transformed_cloud;
+    double rotx = 0.0;
+    double roty = M_PI/2.0;
+    double rotz = -M_PI/2.0;
+
+    rotMatrixX <<
+    1.0, 0.0, 0.0, 0.0,
+    0.0, cos(rotx), -sin(rotx), 0.0,
+    0.0, sin(rotx), cos(rotx), 0.0,
+    0.0, 0.0, 0.0, 1.0;
+
+    rotMatrixY <<
+    cos(roty), 0.0, sin(roty), 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    -sin(roty), 0.0, cos(roty), 0.0,
+    0.0, 0.0, 0.0, 1.0;
+
+    rotMatrixZ <<
+    cos(rotz), -sin(rotz), 0.0, 0.0,
+    sin(rotz), cos(rotz), 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0;
 }
--- laser_transform/src/laser_transform/laser_transform.cpp
+++ laser_transform/src/laser_transform/laser_transform.cpp
@@ -1,9 +1,18 @@
 #include "laser_transform/laser_transform.h"
 
 void My_Filter::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
-    sensor_msgs::PointCloud2 cloud;
-    projector_.transformLaserScanToPointCloud("velodyne", *scan, cloud, tfListener_);
-    point_cloud_publisher_.publish(cloud);
+	sensor_msgs::PointCloud2 cloud;
+	pcl::PointCloud<pcl::pointxyz> pcl_cloud;
+	sensor_msgs::PointCloud2 point_cloud_msg;
+	pcl::PointCloud<pcl::pointxyz> trans_cloud;
+
+	projector_.transformLaserScanToPointCloud("velodyne", *scan, cloud, tfListener_);
+
+	pcl::fromROSMsg(cloud, pcl_cloud);
+	pcl::transformPointCloud(pcl_cloud, trans_cloud, rotMatrixX * rotMatrixY * rotMatrixZ);
+	pcl::toROSMsg(trans_cloud, point_cloud_msg);
+	
+	point_cloud_publisher_.publish(point_cloud_msg);
 }
 
 
