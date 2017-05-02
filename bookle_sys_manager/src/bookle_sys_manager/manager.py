07bc8da107fbcf622755ff8551b0b004e732a028
yswhynot <yswhynot@gmail.com>
2017-05-02 19:59:45 +0800

    add rospy manager

---
+++ bookle_sys_manager/src/bookle_sys_manager/manager.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+def init_io():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(18, GPIO.OUT)
+
+def led_blink():
+	while:
+		GPIO.output(18, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(18, GPIO.LOW)
+		time.sleep(0.5)
+
+def start():
+    print('System manager started\n')
+    init_io()
+    led_blink()
