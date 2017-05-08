da9bb1f64a475c197983bddc5e46dd24f5bcd321
yswhynot <yswhynot@gmail.com>
2017-05-08 17:18:44 +0800

    update

--- hardware_bridge/src/hardware_bridge/bridge.py
+++ hardware_bridge/src/hardware_bridge/bridge.py
@@ -3,6 +3,7 @@ import tf
 
 import time
 import math
+import numpy as np
 
 def init():
 	rospy.init_node('hardware_bridge', anonymous=True)
@@ -26,38 +27,61 @@ def update_transform(listener, tl, tr):
 	try:
 		(trans, rot) = listener.lookupTransform('/odom', '/base_footprint', rospy.Time(0))
 	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-		print('TF lookup fail\n')
+		rospy.logwarn('TF lookup fail\n')
 		break
 
 	x_trans = trans[0]
 	y_trans = trans[1]
-	x_rot = rot[0]
-	y_rot = rot[1]
-	z_rot = rot[2]
-	w_rot = rot[3]
+	euler = tf.transformations.euler_from_quaternion(rot)
+	yaw = eular[2]
 
 	# Calculate differetial drive params
 	l = 0.43
-	angle = 0
+	w = 0
 	path_r = 0
 	icc = (0, 0)
 	(dl, tmp_tl) = get_left_distance
 	(dr, tmp_tr) = get_right_distance
-	vl = dl / (tmp_tl - tl)
-	vr = dr / (tmp_tr - tr)
-	angle = (vr - vl) / l
-	if dl != dr:
+	vl = dl / (tmp_tl - tl).to_sec()
+	vr = dr / (tmp_tr - tr).to_sec()
+	w = (vr - vl) / l
+	if vl != vr:
 		path_r = l * (vl + vr) / (2 * (vr - vl))
 	else:
 		path_r = -1
 
+	# Straight line motion
+	if path_r == -1:
+		trans[0] += (dl + dr) / 2 * math.cos(yaw)
+		trans[1] += (dl + dr) / 2 * math.sin(yaw)
+	else:
+		icc[0] = x_trans - (path_r * math.sin(yaw))
+		icc[1] = y_trans + (path_r * math.cos(yaw))
 
+		time_dif = ((tmp_tl - tl).to_sec() + (tmp_tr - tr).to_sec()) / 2
+		angle_dif = w * time_dif
+		dif_vec = np.array([icc[0], icc[1], angle_dif])
+		origin_vec = np.array([x_trans - icc[0], y - icc[1], yaw])
+		rot_mat = np.matrix(
+			[math.cos(angle_dif), -math.sin(angle_dif), 0],
+			[math.sin(angle_dif), math.cos(angle_dif), 0],
+			[0, 0, 1])
+		result = np.add(np.dot(rot_mat, origin_vec), dif_vec).tolist()
+
+		trans[0] = result[0]
+		trans[1] = result[1]
+		rot = tf.transformations.quaternion_from_euler(0, 0, result[2])
+
+	br = tf.TransformBroadcaster()
+	br.sendTransform(trans, rot, rospy.Time.now(), 'base_footprint', 'odom')
+
+	rospy.loginfo('Transform updated!\n')
 
 	return (tmp_tl, tmp_tr)
 
 
 def start():
-	print('Hardware bridge started :)\n')
+	rospy.loginfo('Hardware bridge started :)\n')
 	init()
 
 	rate = rospy.Rate(10)
