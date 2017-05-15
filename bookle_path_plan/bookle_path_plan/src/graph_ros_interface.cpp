bf6aa60dafc4ce62b72c52cab8902e2a3c1ecec0
yswhynot <yswhynot@gmail.com>
2017-05-16 06:51:08 +0800

    add a filter that does not work

--- bookle_path_plan/bookle_path_plan/src/graph_ros_interface.cpp
+++ bookle_path_plan/bookle_path_plan/src/graph_ros_interface.cpp
@@ -8,21 +8,66 @@ namespace bookle {
 		height = map.info.height;
 		resolution = map.info.resolution;
 		// std::vector<int> data = map.data;
+		printf("resolution: %f\n", resolution);
+		printf("origin: %f, %f, %f\n", map.info.origin.position.x, map.info.origin.position.y, map.info.origin.position.z);
 
 		bVertexSet barrier_set;
+
+		std::vector<Point> point_vec;
+		long unsigned int x_min = width;
+		long unsigned int y_min = height;
+		long unsigned int x_total = 0;
+		long unsigned int y_total = 0;
+		long unsigned int count = 0;
+		const float FILTER_THRESHOLD = 0.3;
 
 		ROS_INFO("\nUpdate barrier of width: %d, height: %d \n", width, height);
 		for(long unsigned int x = 0; x < width; x++) {
 			for(long unsigned int y = 0; y < height; y++) {
 				if(map.data[width * x + y] > BARRIER_THRESHOLD) {
 					// Update grid with all 4 dimentions
-					printf("(%lu, %lu)\n", x, y);
-					for(long unsigned int z = 0; z < Z_LENGTH; z++) {
-						barrier_set.insert(bVertexDescriptor {{x, y, z}});
-					}
+					// printf("(%lu, %lu) ", x, y);
+
+					x_total += x;
+					y_total += y;
+					count ++;
+
+					point_vec.push_back(Point{x, y, 0});
 				}
 			} // end for y
 		} // end for x
+
+		long unsigned int x_avg = x_total / count;
+		long unsigned int y_avg = y_total / count;
+
+		printf("x_avg: %lu, y_avg: %lu\n", x_avg, y_avg);
+		printf("x_th: %lu, y_th: %lu\n", (long unsigned int)(FILTER_THRESHOLD * (float)x_avg), (long unsigned int)(FILTER_THRESHOLD * (float)y_avg));
+
+		// Filter for the physical shit
+		// for(std::vector<Point>::iterator it = point_vec.begin(); it != point_vec.end();) {
+		// 	if(it->x < (long unsigned int)(FILTER_THRESHOLD * (float)x_avg)) {
+		// 		it = point_vec.erase(it);
+		// 	}
+		// 	else if(it->y < (long unsigned int)(FILTER_THRESHOLD * (float)y_avg)) {
+		// 		it = point_vec.erase(it);
+		// 	}
+		// 	else {
+		// 		if(it->x < x_min) x_min = it->x;
+		// 		if(it->y < y_min) y_min = it->y;
+		// 		it++;
+		// 	}
+		// }
+		// printf("xmin: %lu, ymin: %lu\n", x_min, y_min);
+		for(std::vector<Point>::iterator it = point_vec.begin(); it != point_vec.end(); it++) {
+			printf("(%lu, %lu) ", it->x, it->y);
+			// if((it->x < 100) || (it->y < 100)) continue;
+			for(long unsigned int z = 0; z < Z_LENGTH; z++) {
+				// barrier_set.insert(bVertexDescriptor {{it->x, it->y, z}});
+				// barrier_set.insert(bVertexDescriptor {{it->x - x_min, it->y - y_min, z}});
+				// barrier_set.insert(bVertexDescriptor {{it->x - 100, it->y - 100, z}});
+				barrier_set.insert(bVertexDescriptor {{(long unsigned int)(resolution * (float)it->x * 5), (long unsigned int)(resolution * (float)it->y * 5), z}});
+			}
+		}
 
 		if(grid_graph.UpdateBarrier(barrier_set)) {
 			return true;
